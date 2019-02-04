aed74a0aa09b1f1022e647525f282d491814fded
Alan Hay <alanhay99@hotmail.com>
2019-02-04 11:36:40 +0000

    Removed manual controller logic.

--- src/main/java/com/qikserve/hackdazespringdata/HackDazeSpringDataApplication.java
+++ src/main/java/com/qikserve/hackdazespringdata/HackDazeSpringDataApplication.java
@@ -5,13 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
-import com.qikserve.hackdazespringdata.domain.BaseEntity;
-import com.qikserve.hackdazespringdata.domain.Customer;
-import com.qikserve.hackdazespringdata.domain.Order;
-import com.qikserve.hackdazespringdata.domain.controller.mixin.BaseMixin;
-import com.qikserve.hackdazespringdata.domain.controller.mixin.CustomerMixin;
-import com.qikserve.hackdazespringdata.domain.controller.mixin.OrderMixin;
-
 @SpringBootApplication
 public class HackDazeSpringDataApplication {
 
@@ -22,9 +15,9 @@ public class HackDazeSpringDataApplication {
 	@Bean
 	public Jackson2ObjectMapperBuilder objectMapperBuilder() {
 		Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
-		builder.mixIn(BaseEntity.class, BaseMixin.class);
-		builder.mixIn(Customer.class, CustomerMixin.class);
-		builder.mixIn(Order.class, OrderMixin.class);
+		//builder.mixIn(BaseEntity.class, BaseMixin.class);
+		//builder.mixIn(Customer.class, CustomerMixin.class);
+		//builder.mixIn(Order.class, OrderMixin.class);
 
 		return builder;
 	}
